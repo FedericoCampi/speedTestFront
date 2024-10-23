873ae433dee51e1f7835f1f35f4c2f421b34e7a2
FedericoCampi <federicocampi95@gmail.com>
2024-10-23 20:41:59 -0300

    pruebo en consola mensaje del env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-
-
 export default function Home() {
   console.log(process.env.MY_SECRET_VARIABLE);
   
   return (
     <div className="">
-      
+      {process.env.MY_SECRET_VARIABLE}
     </div>
   );
 }
