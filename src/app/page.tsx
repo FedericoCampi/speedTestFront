6819562afa3bf5f9b8be3f4e1f0a3da8d2a56728
FedericoCampi <federicocampi95@gmail.com>
2024-10-23 21:07:49 -0300

    migracion de proyecto speedtest a este

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import FuturisticSpeedTest from "../components/FuturisticSpeedTest";
+
 export default function Home() {
-  console.log(process.env.MY_SECRET_VARIABLE);
   
   return (
     <div className="">
-      {process.env.MY_SECRET_VARIABLE}
+      <FuturisticSpeedTest/>
     </div>
   );
 }
